b44a9d6d6459b6c495ca18049e9c5773b28203fe
iker <ikerhv9@gmail.com>
2023-02-06 12:20:01 +0100

    [ADD] Modificacion Escena y enemigo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Vector3 destination;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveTo (gameObject, iTween.Hash ("position", destination, "time", time, "easetype", iTween.EaseType.easeInOutSine, "looptype", iTween.LoopType.pingPong));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
